17077e5aa110f3d31cca060813ecd609d9b2b931
sAyusha <ayushastha69@gmail.com>
2024-02-02 13:27:01 +0545

    renewaskin app

--- ML/Skin_metrics/Skin_type/model.py
+++ ML/Skin_metrics/Skin_type/model.py
@@ -1,52 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import RMSprop
 
-print('Loading model ...')
-model = tf.keras.models.load_model('saved_model')
 
-class_names = ['Dry_skin','Normal_skin','Oil_skin']
+# In[2]:
 
-def load_and_prep_image(filename, img_shape=224):
-  img = tf.io.read_file(filename)
-  # Decode it into a tensor
-  img = tf.image.decode_jpeg(img)
-  # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
-  # Rescale the image (get all values between 0 and 1)
-  img = img/255.
-  return img
 
-def predict_class(filename):
-  """
-  Imports an image located at filename, makes a prediction with model
-  and plots the image with the predicted class as the title.
-  """
-  print('Loading image ...')
-  # Import the target image and preprocess it
-  img = load_and_prep_image(filename)
-  
-  print('Predicting class of image ...')
+train = ImageDataGenerator(rescale = 1/255)
+validation = ImageDataGenerator(rescale = 1/255)
 
-  # Make a prediction
-  pred = model.predict(tf.expand_dims(img, axis=0))
-  print(pred)
 
-  #Save the trained model
-  model.save('my_model.h5')
+# In[5]:
 
-  #Convert the model to TensorFlow Lite format
-  converter = tf.lite.TFLiteConverter.from_keras_model(model)
-  tflite_model = converter.convert()
 
-  #Save the TensorFlow Lite model to a file
-  with open('my_model.tflite', 'wb') as f:
+train_dataset = train.flow_from_directory("C:/Users/Dell/Downloads/skin_type/train/",
+                                         target_size=(200, 200),
+                                         batch_size=3,
+                                         class_mode='categorical')
+
+validation_dataset = train.flow_from_directory("C:/Users/Dell/Downloads/skin_type/validation/",
+                                         target_size=(200, 200),
+                                         batch_size=3,
+                                         class_mode='categorical')
+
+
+# In[6]:
+
+
+train_dataset.class_indices
+
+
+# In[7]:
+
+
+model = tf.keras.models.Sequential([ tf.keras.layers.Conv2D(16,(3,3), activation = 'relu', input_shape = (200,200,3)),
+                                    tf.keras.layers.MaxPool2D(2,2),
+                                    #
+                                    tf.keras.layers.Conv2D(32,(3,3), activation = 'relu'),
+                                    tf.keras.layers.MaxPool2D(2,2),
+                                    #
+                                    tf.keras.layers.Conv2D(64,(3,3), activation = 'relu'),
+                                    tf.keras.layers.MaxPool2D(2,2),
+                                    ##
+                                    tf.keras.layers.Flatten(),
+                                    ##
+                                    tf.keras.layers.Dense(512,activation= 'relu'),
+                                    ##
+                                    tf.keras.layers.Dense(3,activation='softmax')
+                                    
+                                    
+    
+])
+
+
+# In[8]:
+
+
+model.compile(loss= 'categorical_crossentropy',
+             optimizer = RMSprop(learning_rate=0.001),
+              metrics = ['accuracy']
+             )
+
+
+# In[9]:
+
+
+try:
+    model_fit = model.fit(
+        train_dataset,
+        steps_per_epoch=len(train_dataset),
+        epochs=10,
+        validation_data=validation_dataset
+    )
+except Exception as e:
+    print("Error during training:", e)
+
+
+# In[10]:
+    
+    model.save('skin_type_trained_model.h5')
+
+# Convert the model to TensorFlow Lite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the TensorFlow Lite model to a file
+with open('skintype_trained_model.tflite', 'wb') as f:
     f.write(tflite_model)
 
-  # Add in logic for multi-class & get pred_class name
-  if len(pred[0]) > 1:
-    pred_class = class_names[tf.argmax(pred[0])]
-  else:
-    pred_class = class_names[int(tf.round(pred[0]))]
-  print('Predicted class:', pred_class)
-  return pred_class
 
-predict_class('test_image.jpg')
+
+dir_path = 'C:/Users/Dell/Downloads/skin_type/test'
+
+for i in os.listdir(dir_path):
+    img_path = os.path.join(dir_path, i)
+    img = image.load_img(img_path, target_size=(200, 200))
+    plt.imshow(img)
+    plt.show()
+
+    X = image.img_to_array(img)
+    X = np.expand_dims(X, axis=0)
+    images = np.vstack([X])
+    val = model.predict(images)
+
+    predicted_class = np.argmax(val)  # Get the index of the highest probability class
+
+    if predicted_class == 0:
+        print('Skin Type: Dry')
+    elif predicted_class == 1:
+        print('Skin Type: Normal')
+    else:
+        print('Skin Type: Oily')
+
+
+# In[ ]:
+
+
+
+
